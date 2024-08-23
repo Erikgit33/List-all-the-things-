cd1922a0062d4a1ee07b213f7b9c07f4a8f55f70
Erik Bäckström <Erik Bckstrm@DESKTOP-MEVT6FK>
2024-08-23 14:29:47 +0200

    Add project files.

---
+++ List all the things!/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace List_all_the_things_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.textBoxAdd = new System.Windows.Forms.TextBox();
+            this.buttonList = new System.Windows.Forms.Button();
+            this.buttonUpper = new System.Windows.Forms.Button();
+            this.textBoxContains = new System.Windows.Forms.TextBox();
+            this.buttonContains = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.textBoxRemove = new System.Windows.Forms.TextBox();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(77, 33);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(107, 12);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(77, 33);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonCount
+            // 
+            this.buttonCount.Location = new System.Drawing.Point(12, 83);
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.Size = new System.Drawing.Size(77, 33);
+            this.buttonCount.TabIndex = 2;
+            this.buttonCount.Text = "Count";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Location = new System.Drawing.Point(107, 83);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(77, 33);
+            this.buttonFirst.TabIndex = 3;
+            this.buttonFirst.Text = "First";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            // 
+            // textBoxAdd
+            // 
+            this.textBoxAdd.Location = new System.Drawing.Point(12, 51);
+            this.textBoxAdd.Name = "textBoxAdd";
+            this.textBoxAdd.Size = new System.Drawing.Size(172, 26);
+            this.textBoxAdd.TabIndex = 11;
+            // 
+            // buttonList
+            // 
+            this.buttonList.Location = new System.Drawing.Point(11, 159);
+            this.buttonList.Name = "buttonList";
+            this.buttonList.Size = new System.Drawing.Size(172, 33);
+            this.buttonList.TabIndex = 12;
+            this.buttonList.Text = "List all the things!";
+            this.buttonList.UseVisualStyleBackColor = true;
+            this.buttonList.Click += new System.EventHandler(this.buttonList_Click);
+            // 
+            // buttonUpper
+            // 
+            this.buttonUpper.Location = new System.Drawing.Point(12, 198);
+            this.buttonUpper.Name = "buttonUpper";
+            this.buttonUpper.Size = new System.Drawing.Size(172, 33);
+            this.buttonUpper.TabIndex = 13;
+            this.buttonUpper.Text = "To upper case";
+            this.buttonUpper.UseVisualStyleBackColor = true;
+            this.buttonUpper.Click += new System.EventHandler(this.buttonUpper_Click);
+            // 
+            // textBoxContains
+            // 
+            this.textBoxContains.Location = new System.Drawing.Point(107, 241);
+            this.textBoxContains.Name = "textBoxContains";
+            this.textBoxContains.Size = new System.Drawing.Size(77, 26);
+            this.textBoxContains.TabIndex = 14;
+            // 
+            // buttonContains
+            // 
+            this.buttonContains.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonContains.Location = new System.Drawing.Point(12, 237);
+            this.buttonContains.Name = "buttonContains";
+            this.buttonContains.Size = new System.Drawing.Size(77, 33);
+            this.buttonContains.TabIndex = 15;
+            this.buttonContains.Text = "Contains";
+            this.buttonContains.UseVisualStyleBackColor = true;
+            this.buttonContains.Click += new System.EventHandler(this.buttonContains_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(12, 276);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(172, 33);
+            this.buttonSort.TabIndex = 16;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRemove.Location = new System.Drawing.Point(12, 315);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(77, 33);
+            this.buttonRemove.TabIndex = 17;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // textBoxRemove
+            // 
+            this.textBoxRemove.Location = new System.Drawing.Point(107, 318);
+            this.textBoxRemove.Name = "textBoxRemove";
+            this.textBoxRemove.Size = new System.Drawing.Size(77, 26);
+            this.textBoxRemove.TabIndex = 18;
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(106, 122);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(77, 33);
+            this.buttonLast.TabIndex = 19;
+            this.buttonLast.Text = "Last";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.buttonLast_Click);
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRandom.Location = new System.Drawing.Point(12, 122);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(77, 33);
+            this.buttonRandom.TabIndex = 20;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(190, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(265, 332);
+            this.richTextBox.TabIndex = 21;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 360);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.textBoxRemove);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonContains);
+            this.Controls.Add(this.textBoxContains);
+            this.Controls.Add(this.buttonUpper);
+            this.Controls.Add(this.buttonList);
+            this.Controls.Add(this.textBoxAdd);
+            this.Controls.Add(this.buttonFirst);
+            this.Controls.Add(this.buttonCount);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.TextBox textBoxAdd;
+        private System.Windows.Forms.Button buttonList;
+        private System.Windows.Forms.Button buttonUpper;
+        private System.Windows.Forms.TextBox textBoxContains;
+        private System.Windows.Forms.Button buttonContains;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.TextBox textBoxRemove;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ List all the things!/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_all_the_things_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> list = new List<string>();
+
+        Random random = new Random();
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            list.Add(textBoxAdd.Text);
+            textBoxAdd.Text = "";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                list.Clear();
+            }
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                MessageBox.Show("Count: " + list.Count.ToString());
+            }
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                MessageBox.Show("First: " + list[0].ToString());
+            }
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                MessageBox.Show("Last: " + list[list.Count - 1].ToString());
+            }
+
+            richTextBox.Text = "";
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                MessageBox.Show(list[random.Next(list.Count - 1)] + "!");
+            }
+        }
+
+        private void buttonList_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                for (int listindex = 0; listindex < list.Count; listindex++)
+                {
+                    richTextBox.Text += list[listindex] + " ";
+                }
+            }
+        }
+
+        private void buttonUpper_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = "";
+
+            if (list.Count != 0)
+            {
+                for (int listindex = 0; listindex < list.Count; listindex++)
+                {
+                    richTextBox.Text += list[listindex].ToUpper();
+                }
+            }
+        }
+
+        private void buttonContains_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                int containedText = 0;
+
+                for (int listindex = 0; listindex < list.Count; listindex++)
+                {
+                    if (list[listindex] == textBoxContains.Text)
+                    {
+                        containedText++;
+                    }
+                }
+
+                if (containedText == 0)
+                {
+                    MessageBox.Show("The list does not contain " + textBoxContains.Text + "!");
+                }
+                else
+                {
+                    MessageBox.Show("The list contains " + containedText.ToString() + " " + textBoxContains.Text + "!");
+                }
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = "";
+
+            list.Sort();
+
+            if (list.Count != 0)
+            {
+                for (int listindex = 0; listindex < list.Count; listindex++)
+                {
+                    richTextBox.Text += list[listindex] + " ";
+                }
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (textBoxRemove.Text != "")
+            {
+                richTextBox.Text = "";
+
+                if (list.Count != 0)
+                {
+                    int indexToRemove = 0;
+                    int.TryParse(textBoxRemove.Text, out indexToRemove);
+
+                    for (int listindex = 0; listindex < list.Count; listindex++)
+                    {
+                        if (listindex == indexToRemove)
+                        {
+                            list.RemoveAt(listindex);
+                        }
+                    }
+
+                    for (int listindex = 0; listindex < list.Count; listindex++)
+                    {
+                        richTextBox.Text += list[listindex] + " ";
+                    }
+                }
+            }
+        }
+    }
+}
